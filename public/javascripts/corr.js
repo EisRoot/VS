13f1008d8810abe2352b40b0411eb3b4117deea7
WangTingTing <673950738@qq.com>
2018-11-21 19:28:35 +0800

    update demo

--- public/javascripts/corr.js
+++ public/javascripts/corr.js
@@ -4,20 +4,27 @@ var myChart = echarts.init(document.getElementById('corr'));
 // 指定图表的配置项和数据
 var option = {
     title: {
-        text: 'ECharts 入门示例'
+        text: 'corr'
     },
     tooltip: {},
     legend: {
-        data:['销量']
+        data:['corr']
     },
     xAxis: {
-        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        show:false
+
     },
-    yAxis: {},
+    yAxis: {
+        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"],
+        axisLabel:{
+            show:false
+        }
+
+    },
     series: [{
         name: '销量',
         type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [5, -20, 36, 10, -10, 20]
     }]
 };
 
