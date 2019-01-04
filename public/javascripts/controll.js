4d571956eedc6ecdbe22a5d06270c144de0830eb
WangTingTing <673950738@qq.com>
2019-01-04 17:38:06 +0800

    update demo

---
+++ public/javascripts/controll.js
@@ -0,0 +1,42 @@
+//checkbox延迟判断的逻辑
+var checkMap=new Map();
+var check_box_id=['stu_total_sleep','stu_total_conversation','check_3','check_4'];
+var check_handler=function () {
+    var json;
+    static_option.series=[];
+    static_option.xAxis.data=[];
+    static_Chart.clear();
+
+    checkMap.forEach(function (item, key) {
+        if (item){
+            //statics 试图的变化逻辑
+            var newseries={
+                name:key,
+                type:'line',
+                smooth: true,
+                encoide:{x: 1, y: 0},
+                data:[],
+            };
+            $.get('http://127.0.0.1:3000/json/'+key+'.json',function (data) {
+               
+                echarts.util.each(data, function (dataItem) {
+                    static_option.xAxis.data.push(dataItem[0]);
+                    newseries.data.push(dataItem[1]);
+                });
+                static_option.series.push(newseries);
+                static_Chart.setOption(static_option);
+
+            });
+
+        }
+    })
+
+};
+var check_time_set
+function checkclick(names) {
+    check_box_id.forEach(function (value) {
+        checkMap.set(value,document.getElementById(value).checked);
+    });
+    clearTimeout(check_time_set);
+    check_time_set=setTimeout(check_handler,1000);
+}
--- routes/plot4_deadline.js
+++ routes/plot4_deadline.js
@@ -11,5 +11,11 @@ var jsondata=JSON.stringify(date);
 res.writeHead(200, {'Content-Type': 'application/json'});
 res.end(date);
 });
-
+router.post('/x',function (req, res, next) {
+    var attr_id=req.body.attr_id;
+    console.log(attr_id);
+})
+router.get('/image',function (req, res, next) {
+    res.sendFile()
+})
 module.exports = router;
